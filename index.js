576f112df03475ef1123bb976b3073f345fa09a7
Miweoro <miweoro@outlook.pt>
2025-04-27 22:04:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,133 +1,80 @@
-var io = require('socket.io-client')
-var tbh = require('trollbox-headers').headers()
-var address = "http://www.windows93.net:8081";
-var fs = require('fs')
-var path = require('path')
+var io = require('socket.io-client');
+var tbh = {
+	forceNew: true,
+	transportOptions: {
+		polling: {
+			extraHeaders: {
+				"Accept": "*/*",
+				"Accept-Encoding": "identity",
+				"Accept-Language": "*",
+				"Cache-Control": "no-cache",
+				"Connection": "keep-alive",
+				"Cookie": "",
+				"Host": "www.windows93.net",
+				"Origin": "http://www.windows93.net",
+				"Pragma": "no-cache",
+				"Referer": 'http://www.windows93.net/trollbox/index.php',
+				"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
+			}
+		}
+	}
+}
+var address = "https://www.windows93.net:8088";
 var he = require('he');
-var started = false
-var prefix = "-"
-var commands = {}
-var onmessagecommands = []
-var onuserleftcommands = []
-var onuserjoinedcommands = []
-var currentname = ""
-var currentcolor = ""
-const socket = "";
+var started = false;
+exports.prefix = "-";
+var commands = {};
+var onmessagecommands = [];
+var onuserleftcommands = [];
+var onuserjoinedcommands = [];
+var currentname = "";
+var currentcolor = "";
+exports.socket = io(address, tbh);
 
-function connect(name, color, prefix, welcomemsg) {
-  currentname = name
-  currentcolor = color
-
-  exports.users = {}
-  const socket = io(address, tbh);
-  exports.updatecolor= function(color) {
-    console.log("Updating color")
-    socket.emit('user joined', currentname, color,"beepboop","")
-    currentcolor = color
-  }
-  exports.updatename= function(name) {
-    console.log("Updating name")
-    socket.emit('user joined', name, currentcolor,"beepboop","")
-    currentname = name
-  }
-  socket.on('_connected', function(data){
-    socket.emit('user joined', name, color,"beepboop","")
-    if(welcomemsg){
-      socket.send(welcomemsg)
-    }
-    prefix = prefix
-    started = true
-    exports.onconnect(socket)
-  })
-  socket.on('disconnect', function(data) {
-    console.log("Failed to connect, retrying...")
-    var started = false
-    var prefix = "-"
-    var commands = {}
-    var onmessagecommands = []
-    var onuserleftcommands = []
-    var onuserjoinedcommands = []
-    var currentname = ""
-    var currentcolor = ""
-    const socket = "";
-    connect(name, color, prefix, welcomemsg)
-  });
-  socket.on('user joined', function(data) {
-    for (let index = 0; index < onmessagecommands.length; index++) {
-      setTimeout(() => {
-        onmessagecommands[index](data);
-      }, 1);
-    }
-  });
-  socket.on('user left', function(data) {
-    for (let index = 0; index < onuserleftcommands.length; index++) {
-      setTimeout(() => {
-        onuserleftcommands[index](data);
-      }, 1);
-    }
-  });
-  
-  socket.on('update users', function (data) {
-  
-    users={};
-    for (var key in data) {
-      if (!users[data[key].home]) {
-        users[data[key].nick] = data[key]
-      }else{
-        users[data[key].nick] = data[key]
-      }
-    exports.users = users
-  }});
-  
-  var uses = 0
-  socket.on('message', function(data) {
-    try{
-      if(String(data)){
-        data.color = he.decode(data.color)
-        data.msg = he.decode(data.msg)
-        data.home = he.decode(data.home)
-        data.nick = he.decode(data.nick).replace(/discord/g,"").replace(/hugs/g,"")
-      
-        for (let index = 0; index < onmessagecommands.length; index++) {
-          setTimeout(() => {
-            onmessagecommands[index](data);
-          }, 1);
+function connect(name, color, prefix) {
+    currentcolor = color;
+    currentname = name;
+    prefix = prefix;
+    exports.socket.emit("user joined", currentname, currentcolor, "", "")
+    exports.socket.on("message", function(data) {
+      if (typeof data.msg !== "string") {
+        console.error("Invalid message received:", data);
+        return; // Exit early if data.msg is undefined or not a string
+    }  
+      exports.username = data.nick
+        exports.userhome = data.home
+        exports.msg = data.msg
+        if (exports.msg.startsWith(prefix)) {
+            const args = exports.msg.slice(prefix.length).trim().split(" ");
+            const command = args.shift(); // Extract command name
+    
+            if (commands[command]) {
+                commands[command](...args); // Execute the command with arguments
+            } else {
+                exports.socket.send(`Unknown command: '${command}'`);
+            }
         }
-      
-        if (!started) return;
-        if (data.msg.startsWith(prefix)) {
-          file = data.msg.toLowerCase().slice(prefix.length).split(' ')[0]
-          if(commands[file]){
-            commands[file](data, socket)
-          }
-        }
-        
-      }
-    }catch{
-      console.log("Error while reading message")
-    }
-  })
+    })
+    
 }
 
-exports.connect = function(name, color, prefix, welcomemsg) {
-  connect(name, color, prefix, welcomemsg)
+exports.connect = function(name, color, prefix) {
+  connect(name, color, prefix)
 }
 
 exports.updateprefix = function(newprefix){
-  prefix = newprefix
+  prefix = newprefix; // doenst work
 }
 
 exports.setcommand = function(command,func){
   commands[command] = func
 }
-
-exports.onmessage = function(func) {
-  onmessagecommands.push(func)
+exports.updatecolor = function(color) {
+  exports.socket.emit("user joined", currentname, color, "", "")
 }
-
-exports.onuserjoined = function(func) {
-  onuserjoinedcommands.push(func)
+exports.updatename = function(name) {
+  exports.socket.emit("user joined", name, currentcolor, "", "")
 }
-exports.onuserleft= function(func) {
-  onuserleftcommands.push(func)
+exports.updatewhole = function(name, color) {
+  exports.socket.emit("user joined", name, color, "", "")
 }
